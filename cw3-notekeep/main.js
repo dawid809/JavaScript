fd157b1e701f55b396937e342dd35d39e462f3af
Dawid Dziad <dawid.dziad@microsoft.wsei.edu.pl>
2020-10-29 13:09:15 +0100

    Kompletny commit zajęć

--- cw3-notekeep/main.js
+++ cw3-notekeep/main.js
@@ -1,61 +1,63 @@
-const localStorageNotesKey= 'notes';
-const notes =[];
+const localStorageNotesKey = 'notes';
+let notes = [];
 
-//nowa notatka
-const note={
-    title:'nowa notatka',
-    content:'treść notatki',
-    coloir:'#ff0000',
+document.querySelector('#noteAdd').addEventListener('click', onNewNote)
+
+function onNewNote() {
+    const title = document.querySelector('#noteTitle').value;
+    const content = document.querySelector('#noteContent').value;
+    console.log(title, content);
+}
+// nowa notatka
+const note = {
+    title: 'nowa notatka',
+    content: 'treść notatki',
+    colour: '#ff0000',
     pinned: false,
-    creaeDate: new Date ()
+    createDate: new Date()
 };
 
-//notatka dodana do tablicy notatek
-notes.push(note)
-notes.push(note)
-notes.push(note)
+// notatka dodana do tablicy notatek
+notes.push(note);
+notes.push(note);
+notes.push(note);
 
-
-
-//tablica zapisana w localStograge
+// tablica zapisana w localStorage
 localStorage.setItem(localStorageNotesKey, JSON.stringify(notes));
 
-//odczytanie tablicy notatek z localStorage
-const notesFromStorage=JSON.parse(localStorage.getItem(localStorageNotesKey));
-notes=notes.map(note=>{
-    note.creaeDate=new Date(note.creaeDate);
+// odczytanie tablicy notatek z localStorage
+const notesFromStorage = JSON.parse(localStorage.getItem(localStorageNotesKey));
+notes = notesFromStorage.map( note => {
+    note.createDate = new Date(note.createDate);
     return note;
-})
+});
 
-//zmiana html-a z poziomu js-a
+
+// zmiana html-a z poziomu js-a - sposób obiektowy
 for (let note of notes) {
-const htmlSection =document.createElement('section')
-const htmlTitle =document.createElement('h1')
-const htmlContent =document.createElement('p')
-const htmlDate =document.createElement('h4')
+    const htmlSection = document.createElement('section');
+    const htmlTitle = document.createElement('h1');
+    const htmlContent = document.createElement('p');
+    const htmlDate = document.createElement('h4');
 
-htmlSection.classList.add('note');
-htmlTitle.innerHTML=note.title;
-htmlContent.innerHTML=note.content;
-htmlDate.innerHTML=note.creaeDate.toLocaleString();
+    htmlSection.classList.add('note');
+    htmlTitle.innerHTML = note.title;
+    htmlContent.innerHTML = note.content;
+    htmlDate.innerHTML = note.createDate.toLocaleString();
 
-htmlSection.appendChild(htmlTitle);
-htmlSection.appendChild(htmlContent);
-htmlSection.appendChild(htmlDate);
-
-const main =document.querySelector('main');
-main.appendChild(htmlSection);
+    htmlSection.appendChild(htmlTitle);
+    htmlSection.appendChild(htmlContent);
+    htmlSection.appendChild(htmlDate);
+    
+    const main = document.querySelector('main');
+    main.appendChild(htmlSection);
 }
-//main.removeChild()
+// usuwanie elementów
+// main.removeChild()
 
-
-for(let note of notes){
-    const htmlNote=`
-    <section class="note">
-    <h1>${note.title}</h1>
-    <p>${note.content}</p>
-    <h4>${note.creaeDate.toLocaleString()}</h4>
-    </section>
-    `;
-    document.body.innerHTML +=htmlNote;
-}
+// notatka:
+// Tytuł
+// Treść
+// Kolor notatki
+// Możliwość przypięcia do góry na liście notatek
+// Datę utworzenia
